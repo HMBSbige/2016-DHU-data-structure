b820fe83af68efa8892136339db2b7b73f3af7e2
Bruce Wayne <admin@bige0.com>
2016-12-12 17:40:34 +0800

    C

--- Map/POJ 3481.cpp
+++ Map/POJ 3481.cpp
@@ -1,55 +1,42 @@
-/***************************/
-/* pku3481 Double Queue
-/* Coded by Guojin Zhu
-/* Run time 516MS
-/* AC on Nov. 6, 2011
-/***************************/
 #include <iostream>
 #include <cstdio>
 #include <map>
+#include <vector>
 using namespace std;
-//////////////////////
-class DoubleQueue{
-private:
-    map<int, int> m;
-    int intServedClient;
-public:
-    void initial(){m.clear();}
-    void addClient(int k, int p){m[p] = k;}
-    void serveHighest(){
-        if(m.size() == 0){
-            intServedClient = 0;
-        }else{
-            intServedClient = (*m.rbegin()).second;
-            m.erase(--m.end());
-        }
-    }  
-    void serveLowest(){
-        if(m.size() == 0){
-            intServedClient = 0;
-        }else{
-            intServedClient = (*m.begin()).second;
-            m.erase(m.begin());
-        }
-    }
-    void printServedClient(){printf("%d\n", intServedClient);}  
-};
-/////////////////////////
-int main(){
-    int r, k, p;
-    DoubleQueue dq;
-    dq.initial();
-    while(scanf("%d", &r)&&(r != 0)){
-        if(r == 1){
-            scanf("%d%d", &k, &p);
-            dq.addClient(k, p);
-        }else if(r == 2){
-            dq.serveHighest();
-            dq.printServedClient();
-        }else if(r == 3){
-            dq.serveLowest();
-            dq.printServedClient();
-        }
-    }
-    return 0;
+
+int main()
+{
+	int command,k,p;
+	map<int, int> q;
+	map<int, int>::iterator it;
+	map<int, int>::reverse_iterator rit;
+
+	while(scanf("%d", &command) && command){
+		switch (command) {
+		case 1:
+			scanf("%d %d", &k, &p);//用cin可能TLE
+			q[p] = k;
+			break;
+		case 2:
+			if (q.empty()) {
+				cout << 0 << endl;
+				continue;//直接跳出switch，进入下一次循环。
+			}
+			rit = q.rbegin();
+			cout << rit->second << endl;
+			q.erase(--rit.base());//q.erase((++rit).base()); 
+			break;
+		case 3:
+			if (q.empty()) {
+				cout << 0 << endl;
+				continue;//直接跳出switch，进入下一次循环。
+			}
+			it = q.begin();
+			cout << it->second << endl;
+			q.erase(it);
+			break;
+		}
+	}
+	//system("pause");
+	return 0;
 }
