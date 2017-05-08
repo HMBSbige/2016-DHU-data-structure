9f762669738e2f54383e4d365e856b09624be6ce
Bruce Wayne <admin@bige0.com>
2017-05-08 17:25:39 +0800

    Coded by Guojin Zhu

--- Binary-Search/UVA 10077(1).cpp
+++ Binary-Search/UVA 10077(1).cpp
@@ -1,29 +1,71 @@
-#include <iostream>
-#include <cstdio>
+/*
+UVa10077 - The Stern-Brocot Number System
+Coded by Guojin Zhu
+Run Time 22 MS
+Accepted on September 21, 2014
+*/
+#include<iostream>
+#include<string>
 using namespace std;
-
-void tree(int Lx, int Ly, int Rx, int Ry, int Tx, int Ty)
-{
-    if (Lx+Rx == Tx && Ly+Ry == Ty) {
-        printf("\n");
-        return;
+///////////////////
+struct Fraction{
+    int m, n;
+    Fraction(int a = 0, int b = 0){m = a; n = b;}
+    bool friend operator == (Fraction f1, Fraction f2){
+        return f1.m*f2.n == f2.m*f1.n;
     }
-    if (((double)(Lx+Rx)/(double)(Ly+Ry)) < ((double)Tx/(double)Ty) )
-        {
-        printf("R");
-        tree(Lx+Rx, Ly+Ry, Rx, Ry, Tx, Ty);
+    bool friend operator < (Fraction f1, Fraction f2){
+        return f1.m*f2.n < f2.m*f1.n;
     }
-    else
-        {
-        printf("L");
-        tree(Lx, Ly, Lx+Rx, Ly+Ry, Tx, Ty);
+};
+///////////////
+class SBNumber{
+private:
+    Fraction x;  // from input
+    string ans;  // for result
+public:
+    bool readCase(){cin >> x.m >> x.n; return (x.m != 1)||(x.n != 1);}
+    void computing();
+    void outAns(){cout << ans << endl;}
+};
+void SBNumber::computing(){
+    Fraction lt = Fraction(0, 1);
+    Fraction rt = Fraction(1, 0);
+    ans.clear();
+    while(lt < rt){
+        Fraction mid = Fraction(lt.m + rt.m, lt.n + rt.n);
+        if(mid == x){
+            break;
+        }else if(mid < x){
+            ans.push_back('R');
+            lt = mid;
+        }else{// mid > x
+            ans.push_back('L');
+            rt = mid;
+        }
     }
 }
-
-int main()
-{
-    int n,m;
-    while (cin >> m>> n && !(m == 1 && n == 1))
-        tree(0, 1, 1, 0, m, n);
+int main(){
+    SBNumber sbn;
+    while(sbn.readCase()){
+        sbn.computing();
+        sbn.outAns();
+    }
     return 0;
 }
+/*
+INPUT
+2 3
+4 3
+5 7
+10 14
+878 323
+1 1
+--------
+OUTPUT
+LR
+RLL
+LRRL
+LRRL
+RRLRRLRLLLLRLRRR
+*/
